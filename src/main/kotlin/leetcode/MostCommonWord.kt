348f0bcb20a0d6b5d97e629333c9735149071341
PSK <wiv821253@gmail.com>
2024-11-19 06:35:14 +0900

    feat: add MostCommonWord

--- src/main/kotlin/leetcode/MostCommonWord.kt
+++ src/main/kotlin/leetcode/MostCommonWord.kt
@@ -1,7 +1,25 @@
 package xyz.pubps.leetcode
 
 class MostCommonWord {
+  /**
+   * 1. Convert the paragraph to lowercase and split it into words
+   *   정규식을 사용하여 문자열을 단어로 분리한다.  
+   * 2. Filter out the banned words
+   *   금지된 단어를 제외한다.
+   * 3. Count the frequency of each word
+   *   각 단어의 빈도를 계산한다.
+   * 4. Return the word with the highest frequency
+   *   가장 빈도가 높은 단어를 반환한다.
+   */
   fun mostCommonWord(paragraph: String, banned: Array<String>): String {
-    return ""
+    val bannedSet = banned.toSet()
+    val wordCount = HashMap<String, Int>()
+
+    paragraph.lowercase()
+    .split("[!?',:;. ]".toRegex())
+    .filter { it.isNotEmpty() && it !in bannedSet }
+    .forEach { wordCount[it] = (wordCount[it] ?: 0) + 1}
+
+    return wordCount.maxByOrNull { it.value }?.key ?: ""
   }
 }
