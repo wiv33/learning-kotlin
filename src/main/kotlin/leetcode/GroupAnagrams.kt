65553a01a415feb5c2a456a4b44cd7d8bf58f917
PSK <wiv821253@gmail.com>
2024-11-20 23:06:52 +0900

    feat: group anagrams

--- src/main/kotlin/leetcode/GroupAnagrams.kt
+++ src/main/kotlin/leetcode/GroupAnagrams.kt
@@ -1,4 +1,21 @@
 package xyz.pubps.leetcode
 
 class GroupAnagrams {
+  /**
+   * 1. sort each string
+   * 2. use the sorted string as a key
+   * 3. put the original string into the list of the key
+   */
+  fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val result = mutableMapOf<String, List<String>>()
+    for (str in strs) {
+      val key = str.toCharArray().sorted().joinToString("")
+      
+      result.getOrPut(key) { mutableListOf() }
+        .also {
+          result[key] = it.plus(str)
+        }
+    }
+    return ArrayList(result.values)
+  }
 }
