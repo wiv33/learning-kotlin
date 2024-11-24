018703428e22d9ed56fbda0968a96a831a57979e
PSK <wiv821253@gmail.com>
2024-11-24 18:32:38 +0900

    feat: refactoring

--- src/main/kotlin/leetcode/LongestPalindrome.kt
+++ src/main/kotlin/leetcode/LongestPalindrome.kt
@@ -1,51 +1,33 @@
 package xyz.pubps.leetcode
 
 class LongestPalindrome {
-  /**
-   * 주어진 문자열에서 가장 긴 팰린드롬 부분 문자열을 찾는 함수
-   */
+  var left = 0
+  var maxLen = 0
   fun longestPalindrome(s: String): String {
-    // 문자열 길이가 2보다 작으면 그대로 반환
-    if (s.length < 2) {
-      return s
+    val strLen = s.length
+    if (strLen < 2) return s
+    
+    for (i in s.indices) {
+      extendPalindrome(s, i, i + 1)
+      extendPalindrome(s, i, i + 2)
     }
     
-    var maxLength = 0 // 가장 긴 팰린드롬의 길이
-    var maxLeft = 0 // 팰린드롬의 시작 인덱스
+    return s.substring(left, left + maxLen)
+  }
+  
+  fun extendPalindrome(str: String, j: Int, k: Int) {
+    var l = j
+    var r = k
     
-    // 문자열의 각 인덱스에 대해 팰린드롬을 검사
-    for (i in s.indices - 1) {
-      var left = i
-      var right = i
-      // 홀수 길이의 팰린드롬 검사
-      while (left >= 0 && right < s.length && s[left] == s[right]) {
-        left--
-        right++
-      }
-      
-      // 최대 길이 업데이트
-      if (maxLength < right - left - 1) {
-        maxLeft = left + 1
-        maxLength = right - left - 1
-      }
-      
-      left = i
-      right = i + 1
-      // 짝수 길이의 팰린드롬 검사
-      while (left >= 0 && right < s.length && s[left] == s[right]) {
-        left--
-        right++
-      }
-      
-      // 최대 길이 업데이트
-      if (maxLength < right - left - 1) {
-        maxLength = right - left - 1
-        maxLeft = left + 1
-      }
+    while (l >= 0 && r < str.length && str[l] == str[r]) {
+      l--
+      r++
     }
     
-    // 가장 긴 팰린드롬 부분 문자열 반환
-    return s.substring(maxLeft, maxLeft + maxLength)
+    if (maxLen < r - l - 1) {
+      left = l + 1
+      maxLen = r - l - 1
+    }
   }
 }
 
